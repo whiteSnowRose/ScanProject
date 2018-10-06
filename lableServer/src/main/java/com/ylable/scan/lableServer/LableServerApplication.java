c184673178d80a27218e86af7053a72865f604f8
cn-qinqin.fu <cn-qinqin.fu@ff.com>
2018-10-06 15:56:22 +0800

    add web server project

---
+++ lableServer/src/main/java/com/ylable/scan/lableServer/LableServerApplication.java
@@ -0,0 +1,12 @@
+package com.ylable.scan.lableServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LableServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LableServerApplication.class, args);
+	}
+}
---
+++ lableServer/src/test/java/com/ylable/scan/lableServer/LableServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ylable.scan.lableServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LableServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
